99c5d9a9b2528d7798500a9fc28abb9ae4a86233
Modem514 <indigea514@gmail.com>
2023-03-23 00:33:55 +0800

    style: rename variable

--- include/station_info.h
+++ include/station_info.h
@@ -8,7 +8,7 @@ namespace trainsys {
 
 struct StationInfo {
     StationID station_id;  // id of the station
-    Time arriving_time;    // arriving time of the station (undefined for starting station)
+    Time arrival_time;     // arrival time of the station (undefined for starting station)
     Time departure_time;   // departure time of the station (undefined for terminal station)
     int ticket_price;      // ticket price from previous station to this one in a particular line
                            // (undefined for starting station)
